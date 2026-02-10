a2c4d8d5d8d1876bb3b541951c796a218de749d3
giuliana041820-creator <giuliana041820@email.com>
2026-02-10 17:37:08 -0500

    Finalizacion de actividad en clase

--- project-management-system-master/app/api/routes.py
+++ project-management-system-master/app/api/routes.py
@@ -19,11 +19,11 @@ def get_task_service() -> TaskService:
 
 def to_http(e: Exception) -> HTTPException:
     if isinstance(e, NotFoundError):
-        return HTTPException(status_code = 404, details=str(e))
+        return HTTPException(status_code = 404, detail=str(e))
     if isinstance(e, InvalidStatusTransition, ValidationError, ValueError):
-        return HTTPException(status_code = 400, details=str(e))
+        return HTTPException(status_code = 400, detail=str(e))
     if isinstance(e, DomainError):
-        return HTTPException(status_code=500, details='Internal Server Error')
+        return HTTPException(status_code=500, detail='Internal Server Error')
 
 @router.post('/projects',response_model = ProjectOut ,status_code = 201)
 def create_project(body: ProjectCreate, service: ProjectService = Depends(get_project_service)):
@@ -35,22 +35,56 @@ def create_project(body: ProjectCreate, service: ProjectService = Depends(get_pr
     
 # TO-DO: GET /projects
 @router.get('/projects', response_model=list[ProjectOut])
-def list_projects(service: ProjectService = Depends(get_project_service)):
+def get_projects(service: ProjectService = Depends(get_project_service)):
     try:
         projects = service.list()
         return [ProjectOut(id=project.id, name=project.name) for project in projects]
     except Exception as e:
         raise to_http(e)    
+    
+
 # TO-DO: GET /projects/{project_id}
 @router.get('/projects/{project_id}', response_model=ProjectOut)
-def get_project(project_id: int, service: ProjectService = Depends(get_project_service)):
+def get_project(project_id: str, service: ProjectService = Depends(get_project_service)):
     try:
         project = service.get(project_id)
         return ProjectOut(id=project.id, name=project.name)
     except Exception as e:
         raise to_http(e)
+
+
 # @router.get('/projects/{project_id}', response_model=ProjectOut)
+@router.delete('/projects/{project_id}', status_code=204)
+def delete_project(project_id: str, service: ProjectService = Depends(get_project_service)):
+    try:
+        service.delete(project_id)
+    except Exception as e:
+        raise to_http(e)
+    
 
 # TO-DO POST /projects/{project_id}/tasks
+@router.post('/projects/{project_id}/tasks', response_model=TaskOut, status_code = 201)
+def create_task(project_id: str, body: TaskCreate, service: TaskService = Depends(get_task_service)):
+    try: 
+        task = service.create(project_id = project_id, title = body.title)
+        return TaskOut(id = task.id, project_id = task.project_id, title= task.title, status = task.status)
+    except Exception as e:
+        raise to_http(e)
+    
 # TO-DO GET /projects/{project_id}/tasks
+@router.get('/projects/{project_id}/tasks', response_model = list [TaskOut])
+def get_project_tasks(project_id: str, service: TaskService = Depends(get_task_service)):
+    try:
+        tasks = service.list_by_project(project_id)
+        return [TaskOut(id=task.id, project_id=task.project_id, title=task.title, status=task.status) for task in tasks]
+    except Exception as e:
+        raise to_http(e)
+
 # TO-DO DELETE /tasks/{task_id}
+@router.delete('/tasks/{task_id}', status_code=204)
+def delete_task(task_id: str, service: TaskService = Depends(get_task_service)):
+    try:
+        service.delete(task_id)
+        return None
+    except Exception as e:
+        raise to_http(e)
